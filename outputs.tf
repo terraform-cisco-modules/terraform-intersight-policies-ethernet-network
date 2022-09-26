903f9d7fa7811092e6da1d503421655ad72fc79c
scotttyso <tyscott@cisco.com>
2022-09-26 04:25:35 -0400

    updates after round 1 testing

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@
 
 output "moid" {
   description = "Ethernet Network Policy Managed Object ID (moid)."
-  value       = intersight_vnic_eth_network_policy.vnic_policy.moid
+  value       = intersight_vnic_eth_network_policy.ethernet_network.moid
 }
